39d914f2e730aee662e12252dcfe3f80bdfe1b51
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2020-07-16 09:12:14 +0300

    Update products

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -23,51 +23,30 @@ export default {
       products: [
         {
           id: 1,
-          name: "Nike 1",
-          price: 10,
-          description: "Awesome Nike shoes",
+          name: "Nike SB Nyjah Free",
+          price: 1000,
+          description:
+            "Dubbed as arguably the most innovative skate shoe in the market today, the Nike SB Nyjah Free displays an unassuming and simple aesthetic that features a lot of technical functionalities for skateboarding.",
           img:
-            "https://i.pinimg.com/236x/7a/ae/e0/7aaee08864597f107bb23deea040f064.jpg"
+            "https://cdn.runrepeat.com/i/nike/28606/nike-sb-nyjah-free-chaussures-de-fitness-mixte-adulte-multicolore-thunderstorm-white-obsidian-400-43-eu-multicolore-thunderstorm-white-obsidian-400-6db0-380.jpg"
         },
         {
           id: 2,
-          name: "Nike 2",
-          price: 10,
-          description: "Awesome Nike shoes",
+          name: "Nike Air Force 1 07",
+          price: 4200,
+          description:
+            "The Nike Air Force 1 07 sneaker is an excellent buy in many ways. Many are complimenting it for its usually monochromatic colorway and neat silhouette. ",
           img:
-            "https://i.pinimg.com/236x/7a/ae/e0/7aaee08864597f107bb23deea040f064.jpg"
+            "https://cdn.runrepeat.com/i/nike/25004/nike-mens-air-force-1-low-07-basketball-shoes-black-black-315122-001-black-black-3cb8-380.jpg"
         },
         {
           id: 3,
-          name: "Nike 3",
+          name: "Nike Air Max 270",
           price: 10,
-          description: "Awesome Nike shoes",
+          description:
+            "This Air Max model ticks off all the boxes concerning comfort, support, superb fit, and lightness, which makes up for its expensive price point.",
           img:
-            "https://i.pinimg.com/236x/7a/ae/e0/7aaee08864597f107bb23deea040f064.jpg"
-        },
-        {
-          id: 4,
-          name: "Nike 4",
-          price: 10,
-          description: "Awesome Nike shoes",
-          img:
-            "https://i.pinimg.com/236x/7a/ae/e0/7aaee08864597f107bb23deea040f064.jpg"
-        },
-        {
-          id: 5,
-          name: "Nike 5",
-          price: 10,
-          description: "Awesome Nike shoes",
-          img:
-            "https://i.pinimg.com/236x/7a/ae/e0/7aaee08864597f107bb23deea040f064.jpg"
-        },
-        {
-          id: 6,
-          name: "Nike 6",
-          price: 10,
-          description: "Awesome Nike shoes",
-          img:
-            "https://i.pinimg.com/236x/7a/ae/e0/7aaee08864597f107bb23deea040f064.jpg"
+            "https://cdn.runrepeat.com/i/nike/28278/nike-air-max-270-desert-sand-hot-punch-volt-total-orange-7d46-380.jpg"
         }
       ]
     };
